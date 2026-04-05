5eebd43c649876a852d00460830771ef05a9ba90
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-05 20:57:03 +0530

    Add POST API to create todo with Pydantic validation and SQLAlchemy insert

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from models import Todos
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -21,6 +22,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]  # give me a Session using get_db()
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priorities: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -33,3 +41,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model:
         return todo_model
     raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
