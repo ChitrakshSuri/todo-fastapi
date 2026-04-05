4dce1066ffdf4bcceb765a4d2ef60a156ff5fd00
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-05 19:22:56 +0530

    Add GET endpoints for all todos and single todo with status codes and path validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -21,6 +21,15 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]  # give me a Session using get_db()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Item not found")
