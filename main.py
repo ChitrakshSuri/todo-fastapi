e8c6bab8d7a0fc3add3589c27b3b7ece3c6e24e6
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-06 01:46:22 +0530

    Refactor main app to include routers instead of defining routes directly

--- main.py
+++ main.py
@@ -1,74 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, Path, status
+from fastapi import FastAPI
 import models
-from models import Todos
 from database import engine, SessionLocal
-from typing import Annotated
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
+from routers import todos, auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db_dependency = Annotated[Session, Depends(get_db)]  # give me a Session using get_db()
-
-
-class TodoRequest(BaseModel):
-    title: str = Field(min_length=3)
-    description: str = Field(min_length=3, max_length=100)
-    priorities: int = Field(gt=0, lt=6)
-    complete: bool
-
-
-@app.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
-
-
-@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-
-    if todo_model:
-        return todo_model
-    raise HTTPException(status_code=404, detail="Item not found")
-
-
-@app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
-
-    db.add(todo_model)
-    db.commit()
-
-
-@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-def edit_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if not todo_model:
-        raise HTTPException(status_code=404, detail="item not found")
-    todo_model.title = todo_request.title
-    todo_model.description = todo_request.description
-    todo_model.priorities = todo_request.priorities
-    todo_model.complete = todo_request.complete
-
-    db.add(todo_model)
-    db.commit()
-
-
-@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(db: db_dependency, todo_id: int, todo_request: TodoRequest):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if not todo_model:
-        raise HTTPException(status_code=404, detail="item not found")
-    db.query(Todos).filter(Todos.id == todo_id).delete()
-    db.commit()
+app.include_router(auth.router)
+app.include_router(todos.router)
