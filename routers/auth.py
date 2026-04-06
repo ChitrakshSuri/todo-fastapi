dca945449996493f16a79b85f1ae4f840e4ce61b
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-07 00:05:05 +0530

    Refactor auth routes with prefix, simplify endpoints, and add proper error handling

--- routers/auth.py
+++ routers/auth.py
@@ -10,13 +10,13 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from datetime import timedelta, datetime, timezone
 
-router = APIRouter()
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 SECRET_KEY = "3f1188bf4a99021805b037bd95727d21e72a3c0aba965c251ee6220cb37797a4"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -78,7 +78,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         )
 
 
-@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -99,7 +99,9 @@ async def login_for_access_token(
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return "Failed Authentication"
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
 
     return {"access_token": token, "token_type": "bearer"}
