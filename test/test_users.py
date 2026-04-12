5790c6d34fc78f0ed302b7adfdbb4c0baf97b8a1
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-12 18:00:53 +0530

    Add user tests for password change and phone number update

--- test/test_users.py
+++ test/test_users.py
@@ -15,3 +15,25 @@ def test_return_user(test_user):
     assert response.json()["last_name"] == "suri"
     assert response.json()["role"] == "admin"
     assert response.json()["phone_number"] == "1234567890"
+
+
+def test_change_password_success(test_user):
+    response = client.put(
+        "/users/change_password",
+        json={"password": "testpassword", "new_password": "newpassword"},
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    response = client.put(
+        "/users/change_password",
+        json={"password": "wrongpassword", "new_password": "newpassword"},
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "Incorrect current password"}
+
+
+def test_change_phone_number_success(test_user):
+    response = client.put("/users/edit_phone_number/0987654321")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
