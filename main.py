d037ab92cd677507feb78dcc2e0c836d12bbe48a
Chitraksh Suri <chitraksh.suri@prismberry.com>
2026-04-13 23:03:10 +0530

    Add Jinja2 template rendering and static file serving to main app, including root route for home page

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import models
 from database import engine, SessionLocal
 from routers import todos, auth, admin, users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def test(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html")
 
 
 @app.get("/healthy")
